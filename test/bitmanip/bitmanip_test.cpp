174367dec999b9f55a2d395f076d34d9ebe770c8
Jiahan Zhang <jzhang@tenstorrent.com>
2024-05-03 23:22:05 -0500

    Add unit test for std::array slicing.

--- test/bitmanip/bitmanip_test.cpp
+++ test/bitmanip/bitmanip_test.cpp
@@ -51,6 +51,38 @@ TEST(Bitmanip, ArraySliceBitset) {
 
 }
 
+TEST(Bitmanip, ArraySliceArray) {
+
+     for (int start = 0; start < 64; start++) {
+        for (int width = 64; width <= 128; width++) {
+            typedef std::array<uint32_t, (128+64)/32> T;
+
+            std::uint32_t arr[(128+64)/32] = {0,};
+
+            int lsb = start;
+            int msb = start + width - 1;
+
+            setbit(arr, lsb);
+            setbit(arr, msb);
+
+            T expected{};
+            setbit(expected.data(), 0);
+            setbit(expected.data(), width - 1);
+
+            T actual = cvm::bitmanip::array_slice<T, 32>(arr, msb, lsb);
+            EXPECT_EQ(actual, expected);
+
+            actual = cvm::bitmanip::array_slice<T, 16>(arr, msb, lsb);
+
+            T expected2{};
+            setbit(expected2.data(), 0);
+            setbit(expected2.data(), (width - 1) + 16*((width - 1)/16));
+            EXPECT_EQ(actual, expected2);
+        }
+    }
+
+}
+
 TEST(Bitmanip, VectorByteFundamentalSlice) {
 
     for (int start = 0; start < 64; start++) {

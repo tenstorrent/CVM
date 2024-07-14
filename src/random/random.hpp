5ee4a555ad9285adc052b1b206a79cd71c4bb976
Mansoor Anees <manees@tenstorrent.com>
2024-07-14 10:20:40 -0500

    Support cvm::rand::get(FLAGS_flag)
    cvm_rand::get("flag") for SV
    FLAGS_flag has to be a string flag in the format "min:max"
    cvm::rand::seed(FLAGS_seed) to set the seed

--- src/random/random.hpp
+++ src/random/random.hpp
@@ -3,15 +3,22 @@
 
 namespace cvm {
 
-    template<typename T, T LO = std::numeric_limits<T>::min(), T HI = std::numeric_limits<T>::max()>
-    struct rng {
-        static_assert(std::is_integral_v<T>, "T must be an integral type for uniform_int_distribution");
-        std::mt19937 gen;
-        std::uniform_int_distribution<T> distrib;
-        rng(uint64_t seed) : gen(seed), distrib(LO, HI) {}
-        T operator()() {
-            return distrib(gen);
-        }
-    };
+    namespace rand {
+
+        template<typename T, T LO = std::numeric_limits<T>::min(), T HI = std::numeric_limits<T>::max()>
+        struct rng {
+            static_assert(std::is_integral_v<T>, "T must be an integral type for uniform_int_distribution");
+            std::mt19937 gen;
+            std::uniform_int_distribution<T> distrib;
+            rng() : distrib(LO, HI) {}
+            T operator()() {
+                return distrib(gen);
+            }
+        };
+
+        void seed(uint64_t seed);
+        uint32_t get(std::string flag);
+        std::pair<uint32_t,uint32_t> parse(std::string flag);
+    }
 
 }
